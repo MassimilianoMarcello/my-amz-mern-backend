1acc68ed5e69cc29acac86d9cd151a7fbfbd9eec
massimiliano <massimarcello@hotmail.it>
2025-01-19 15:43:25 +0100

    Aggiunta configurazione per Render con porta dinamica

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ import paymentRoutes from './routes/payment.js';
 
 // load environment variables
 dotenv.config();
-const PORT = process.env.PORT || 5003;
+
+const PORT = process.env.PORT || 5004;
 
 // connect to database
 connectToDB();
